44a2dc949c4e4509c8b6c0026cc8ff19ea7d6a20
Anonim19992 <acgyf11@mail.ru>
2023-12-10 17:40:49 +1000

    telegram_bot_v0.2 (add button financial indicators)

--- bot.py
+++ bot.py
@@ -12,16 +12,38 @@ def start_message(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("Состояние авто")
     item2 = types.KeyboardButton("Поиск на карте")
-    markup.add(item1, item2)
+    item3 = types.KeyboardButton("Финансовые показатели")
+    markup.add(item1, item2, item3)
     bot.send_message(message.chat.id, 'Выберите что вам надо', reply_markup=markup)
 
 @bot.message_handler(content_types='text')
 def message_reply(message):
+    if message.text == "Финансовые показатели":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("Таблица")
+        item2 = types.KeyboardButton("Круговая диаграмма")
+        item3 = types.KeyboardButton("График рассеяния")
+        item4 = types.KeyboardButton("Гистограмма")
+        item5 = types.KeyboardButton("Назад")
+        markup.add(item1, item2, item3, item4, item5)
+        bot.send_message(message.chat.id, 'Выберите что вам надо', reply_markup=markup)
     if message.text == "Состояние авто":
         photo = open('Состояние авто.JPG', 'rb')
         bot.send_photo(message.chat.id, photo)
     if message.text == "Поиск на карте":
         photo = open('Map.JPG', 'rb')
+        bot.send_photo(message.chat.id, photo)
+    if message.text == "Таблица":
+        photo = open('Таблица.png', 'rb')
+        bot.send_photo(message.chat.id, photo)
+    if message.text == "Круговая диаграмма":
+        photo = open('Круговой график.png', 'rb')
+        bot.send_photo(message.chat.id, photo)
+    if message.text == "График рассеяния":
+        photo = open('График рассеяния.png', 'rb')
+        bot.send_photo(message.chat.id, photo)
+    if message.text == "Гистограмма":
+        photo = open('Гистограмма.png', 'rb')
         bot.send_photo(message.chat.id, photo)
 
 bot.polling(none_stop=True, interval=0)
